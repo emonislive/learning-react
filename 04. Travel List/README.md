31ede79b66edad7e549dcabe1cc321e1249e2914
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-10-05 19:55:55 +0600

    05/10/2025

---
+++ 04. Travel List/README.md
@@ -0,0 +1,15 @@
+## Instruction
+
+### Preview
+- Live Preview - https://todolist-z.netlify.app/
+  
+<img src="preview.png"  height="450px" />
+
+<br>
+<br>
+
+- clone the repo then inside the folder run these commands
+```bash
+npm install
+npm start
+```
---
+++ 04. Travel List/src/App.js
@@ -0,0 +1,137 @@
+import { useState } from "react";
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = (packedItems / numItems) * 100;
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+      />
+      <Stats
+        total={numItems}
+        packedItems={packedItems}
+        percentage={percentage}
+      />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <>
+      <h1>Far Away</h1>
+    </>
+  );
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    if (!description) return;
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items, onDeleteItem, onToggleItems }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItems={onToggleItems}
+            key={item.id}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item, onDeleteItem, onToggleItems }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItems(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
+
+function Stats({ total, packedItems, percentage }) {
+  return (
+    <footer className="stats">
+      <em>
+        You have {total} items on your list, and you already packed{" "}
+        {packedItems} {!percentage ? null : percentage}%
+      </em>
+    </footer>
+  );
+}
