ae0587694463905f689d69db0350c524b21b724d
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-10-06 12:19:07 +0600

    06/10/2025

--- Exercise/05. accordion-component/src/AccordionItem.js
+++ Exercise/05. accordion-component/src/AccordionItem.js
@@ -1,12 +1,6 @@
-import { useState } from "react";
-
-export function AccordionItem({ title, text, num }) {
-  const [isOpen, setOpen] = useState(false);
+export function AccordionItem({ title, text, num, isOpen, onToggle }) {
   return (
-    <div
-      className={`item ${isOpen ? "open" : ""}`}
-      onClick={() => setOpen(!isOpen)}
-    >
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={onToggle}>
       <p className="number">{num > 0 && num <= 9 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
